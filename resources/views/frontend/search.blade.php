782a58caf366cfb2524b6ca49a7ce2078776cd63
Đỗ Mạnh Quân <manhquan.do@ved.com.vn>
2016-07-08 13:22:19 +0700

    update

--- resources/views/frontend/search.blade.php
+++ resources/views/frontend/search.blade.php
@@ -7,7 +7,7 @@
         <div class="topevent">
             @if ($firstClub = $clubs->shift())
             <div class="slider_area">
-                <div class="bxpost"> <img src="{{url('img/cache/577x321', $firstClub->image)}}" class="img">
+                <div class="bxpost"> <img src="{{url('img/cache/577x321/' . $firstClub->image)}}" class="img">
                     <div class="article-content"><a href="{{url($firstClub->slug)}}">{{$firstClub->name}}</a></div>
                 </div>
             </div>
@@ -15,7 +15,7 @@
             <div class="beside_slider">
                 @foreach ($clubs as $club)
                 <div class="single-article ">
-                    <div class="bxpost"> <img src="{{url('img/cache/282x157', $club->image)}}" class="img">
+                    <div class="bxpost"> <img src="{{url('img/cache/282x157/'. $club->image)}}" class="img">
                         <div class="article-content"><a href="{{url($club->slug)}}">{{$club->name}}</a></div>
                     </div>
                 </div>
