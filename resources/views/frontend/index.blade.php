27f253801ba509e78447281f844abfa278c8fffb
Đỗ Mạnh Quân <manhquan.do@ved.com.vn>
2016-07-11 14:50:12 +0700

    update

--- resources/views/frontend/index.blade.php
+++ resources/views/frontend/index.blade.php
@@ -10,7 +10,7 @@
             <div class="videoWrapper" id="bxvideo">
                 <iframe width="560" height="315" src="{{config('constants.VIDEO_URL')}}" frameborder="0" allowfullscreen></iframe>
             </div>
-            <div class="rowbt"> <a href="#" class="bt mb20px">Tìm ESC trong trường bạn</a>
+            <div class="rowbt"> <a href="{{url('search')}}" class="bt mb20px">Tìm ESC trong trường bạn</a>
                 <p>Bạn có thắc mắc? <a href="{{url('faq')}}">Đọc ngay FAQ</a></p>
             </div>
         </div>
@@ -32,41 +32,22 @@
             <h2 class="title">Hướng nghiệp</h2>
             <div class="cartabs">
                 <ul class="nav nav-pills">
-                    <li class="active"><a data-toggle="pill" href="#tab1"><p><img src="{{url('frontend/images/tab1.png')}}"></p> <p class="txt">Khám phá <br>bản thân</p></a></li>
-                    <li><a data-toggle="pill" href="#tab2"><p><img src="{{url('frontend/images/tab2.png')}}"></p><p class="txt">Học bổng</p></a></li>
-                    <li><a data-toggle="pill" href="#tab3"><p><img src="{{url('frontend/images/tab3.png')}}"></p><p class="txt">Thực tập <br>trong nước</p></a></li>
-                    <li><a data-toggle="pill" href="#tab4"><p><img src="{{url('frontend/images/tab4.png')}}"></p><p class="txt">Thực tập <br> nước ngoài</p></a></li>
+                    @foreach ($orientationCategory->subCategories as $k => $oSub)
+                    <li class="active">
+                        <a data-toggle="pill" href="#tab{{$k + 1}}"><p><img src="{{url('img/cache/112x114', $oSub->image)}}"></p> <p class="txt">{{$oSub->name}}</p></a></li>
+                    @endforeach
+
                 </ul>
                 <div class="tab-content">
-                    <div id="tab1" class="tab-pane fade in active">
-                        <h3>Khám phá bản thân</h3>
-                        <p>Esports là một lĩnh vực vô cùng rộng lớn với rất nhiều ngành nghề phong phú & đa dạng, từ những nghề liên quan trực tiếp tới Esports như tuyển thủ chuyên nghiệp, huấn luyện viên, quản lí đội tuyển Esports... hay những ngành nghề liên quan như truyền thông, marketing, kĩ thuật viên live-stream...<br><br>
-
-                            Hãy cùng khám phá hệ thống ngành nghề Esports mà chúng tôi đã dày công xây dựng nên, tham khảo những case-study thú vị và khám phá tiềm năng của chính bản thân mình nhé!</p>
-                        <div class="rowbt"> <a href="{{url('chuyen-muc', 'kham-pha-ban-than')}}" class="bt mb20px">Tìm hiểu thêm</a> </div>
+                    @foreach ($orientationCategory->subCategories as $k => $oSub)
+                    <div id="tab1" class="{{ ($k == 0) ? 'tab-pane fade in active' : 'tab-pane fade' }}">
+                        <h3>{{$oSub->name}}</h3>
+                        <p>{{$oSub->desc}}</p>
+                        <div class="rowbt"> <a href="{{url('chuyen-muc', $oSub->slug)}}" class="bt mb20px">Tìm hiểu thêm</a> </div>
                     </div>
-                    <div id="tab2" class="tab-pane fade">
-                        <h3>Học bổng</h3>
-                        <p>Esports là một lĩnh vực vô cùng rộng lớn với rất nhiều ngành nghề phong phú & đa dạng, từ những nghề liên quan trực tiếp tới Esports như tuyển thủ chuyên nghiệp, huấn luyện viên, quản lí đội tuyển Esports... hay những ngành nghề liên quan như truyền thông, marketing, kĩ thuật viên live-stream...<br><br>
-
-                            Hãy cùng khám phá hệ thống ngành nghề Esports mà chúng tôi đã dày công xây dựng nên, tham khảo những case-study thú vị và khám phá tiềm năng của chính bản thân mình nhé!</p>
-                        <div class="rowbt"> <a href="{{url('chuyen-muc', 'hoc-bong')}}" class="bt mb20px">Tìm hiểu thêm</a> </div>
-                    </div>
-                    <div id="tab3" class="tab-pane fade">
-                        <h3>Thực tập trong nước</h3>
-                        <p>Esports là một lĩnh vực vô cùng rộng lớn với rất nhiều ngành nghề phong phú & đa dạng, từ những nghề liên quan trực tiếp tới Esports như tuyển thủ chuyên nghiệp, huấn luyện viên, quản lí đội tuyển Esports... hay những ngành nghề liên quan như truyền thông, marketing, kĩ thuật viên live-stream...<br><br>
-
-                            Hãy cùng khám phá hệ thống ngành nghề Esports mà chúng tôi đã dày công xây dựng nên, tham khảo những case-study thú vị và khám phá tiềm năng của chính bản thân mình nhé!</p>
-                        <div class="rowbt"> <a href="{{url('chuyen-muc', 'thuc-tap-trong-nuoc')}}" class="bt mb20px">Tìm hiểu thêm</a> </div>
-                    </div>
-                    <div id="tab4" class="tab-pane fade">
-                        <h3>Thực tập ngoài nước</h3>
-                        <p>Esports là một lĩnh vực vô cùng rộng lớn với rất nhiều ngành nghề phong phú & đa dạng, từ những nghề liên quan trực tiếp tới Esports như tuyển thủ chuyên nghiệp, huấn luyện viên, quản lí đội tuyển Esports... hay những ngành nghề liên quan như truyền thông, marketing, kĩ thuật viên live-stream...<br><br>
-
-                            Hãy cùng khám phá hệ thống ngành nghề Esports mà chúng tôi đã dày công xây dựng nên, tham khảo những case-study thú vị và khám phá tiềm năng của chính bản thân mình nhé!</p>
-                        <div class="rowbt"> <a href="{{url('chuyen-muc', 'thuc-tap-ngoai-nuoc')}}" class="bt mb20px">Tìm hiểu thêm</a> </div>
-                    </div>
+                    @endforeach
                 </div>
+
             </div>
         </div>
     </section>
